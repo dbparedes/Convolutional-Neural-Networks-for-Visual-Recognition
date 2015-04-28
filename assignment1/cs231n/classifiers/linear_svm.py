3b87c42edcdf37076688a30a2c7db3cdd6ea1200
dbparedes <db.paredes@gmail.com>
2015-04-28 17:46:45 -0500

    added the gradient of the SVM loss function

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -20,22 +20,30 @@ def svm_loss_naive(W, X, y, reg):
   num_classes = W.shape[0]
   num_train = X.shape[1]
   loss = 0.0
+  delta = 1
   for i in xrange(num_train):
     scores = W.dot(X[:, i])
     correct_class_score = scores[y[i]]
+    count = 0
+    idx = 0     # to save index
     for j in xrange(num_classes):
       if j == y[i]:
+        idx = j
         continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      margin = scores[j] - correct_class_score + delta # note delta = 1
       if margin > 0:
         loss += margin
-
+        dW[j,:] += X[:,i].transpose()
+        count += 1
+    dW[idx,:] -= count * X[:,i].transpose()  
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -78,6 +86,10 @@ def svm_loss_vectorized(W, X, y, reg):
 
   loss += np.sum(margin)/num_train
   loss += 0.5 * reg * np.sum(W * W)
+  margin[margin>0] = 1
+  dw += margin.dot(X.transpose())
+  dw /= num_train
+  dw += reg * W
     #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
